b058213507a3811d57148546fe3346893b42663a
unknown <decofeco29@gmail.com>
2018-01-31 12:56:53 +0000

    Adding dropdown

--- application/modules/port/controllers/ReportController.php
+++ application/modules/port/controllers/ReportController.php
@@ -20,6 +20,30 @@ class Port_ReportController extends Zend_Controller_Action
     public function otherAction()
     {
         $members = new Application_Model_DbTable_Members();
-        $this->view->members = $members->fetchAll();
+        $where = array('name_f = ?' => 'Steven',
+                       'name_l = ?' => 'Foster',
+                       'phone' => '016222000');
+        $this->view->rows = $members->fetchAll($where);
+        
+        //$select = $members->select();
+        //$select->where('name_f = ?', 'Steven');
+        //$this->view->rows = $members->fetchAll($select);     both versions working
+    }
+
+    public function dateyearAction()
+    {
+        $this->view->currently_selected = date('Y');
+        $this->view->earliest_year = 1950; 
+        $this->view->latest_year = date('Y');
+
+        $this->view->month_array = array("January", "February", "March", "April", "May", "June", "July", "Aughust", "September", "October", "November", "December");
+
+        $this->view->year = "";
+        $this->view->month = "";
+        if ($this->getRequest()->isPost()){
+            $formData = $this->getRequest()->getPost();
+            $this->view->year = $formData['year'];
+            $this->view->month = $formData['month'];
+        }
     }
 }
