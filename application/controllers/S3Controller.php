bbd0cb1dba93ddde16cc9b916e5feafc8d0445c1
Declan Munroe <decofeco29@gmail.com>
2019-01-10 12:02:12 +0000

    Back working

--- application/controllers/S3Controller.php
+++ application/controllers/S3Controller.php
@@ -5,29 +5,6 @@ use Aws\S3\S3Client;
 class S3Controller extends Zend_Controller_Action
 {
     public function indexAction() {
-//        if(isset($_FILES['image'])){
-//		$file_name = $_FILES['image']['name'];   
-//		$temp_file_location = $_FILES['image']['tmp_name']; 
-//
-//		//require 'vendor/autoload.php';
-//
-//		$s3 = new Aws\S3\S3Client([
-//			'region'  => 'eu-west-1',
-//			'version' => 'latest',
-//			'credentials' => [
-//				'key'    => "AKIAIFQ5FAMYGBOJLG2Q",
-//				'secret' => "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",
-//			]
-//		]);		
-//
-//		$result = $s3->putObject([
-//			'Bucket' => 'declan-developer-upload',
-//			'Key'    => $file_name,
-//			'SourceFile' => $temp_file_location			
-//		]);
-//
-//		var_dump($result);
-//	}
         
         $s3Client = S3Client::factory(array(
             'credentials' => array(
@@ -41,7 +18,6 @@ class S3Controller extends Zend_Controller_Action
         foreach ($result['Contents'] as $object) {
             echo $object['Key'];
         }
-        //die(var_dump($result['Contents']['Key']));
     }
 }
 
