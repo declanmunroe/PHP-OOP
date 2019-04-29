9e5af806a8f5c07d63841a9329e4a03a1199d8ee
Declan Munroe <decofeco29@gmail.com>
2019-04-29 15:26:33 +0100

    Get payment intent response

--- application/controllers/StripeelementsController.php
+++ application/controllers/StripeelementsController.php
@@ -1,5 +1,8 @@
 <?php
 
+use Stripe\Stripe;
+use Stripe\PaymentIntent;
+
 class StripeelementsController extends Zend_Controller_Action
 {
     public function init()
@@ -10,6 +13,20 @@ class StripeelementsController extends Zend_Controller_Action
     public function indexAction()
     {  
  
+    }
+    
+    public function createpaymentintentAction()
+    {  
+        $formData = $this->getRequest()->getPost();
+        
+        Stripe::setApiKey("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+
+        $intent_response = PaymentIntent::create([
+          'amount' => $formData['price'],
+          'currency' => 'eur',
+        ]);
+        
+        $this->_helper->json($intent_response);
     }
     
 }
