fbe74df905a209679cafbb13af4468859263c290
Declan Munroe <decofeco29@gmail.com>
2019-09-10 16:00:36 +0100

    Quick and dirty solution

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+ini_set('upload_max_filesize', '20M');
+ini_set('post_max_size', '21M');
+
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
