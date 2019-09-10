caade5d44fb73f6ac31feb411718bd5ba256f099
Declan Munroe <decofeco29@gmail.com>
2019-09-10 16:09:35 +0100

    Change limit

--- public/index.php
+++ public/index.php
@@ -4,8 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-ini_set('upload_max_filesize', '20M');
-ini_set('post_max_size', '21M');
+// https://stackoverflow.com/questions/28897152/increase-the-limit-of-file-upload-size-in-heroku-while-uploading-to-dropbox
+ini_set('upload_max_filesize', '10M');
+ini_set('post_max_size', '10M');
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
