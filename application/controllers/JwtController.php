c310c03d10a092f260f9217a20d9ccc78de389f8
Declan <declan.munroe@ics.ie>
2020-04-02 12:09:45 +0100

    Generate jwt token

--- application/controllers/JwtController.php
+++ application/controllers/JwtController.php
@@ -1,8 +1,34 @@
 <?php
 
+use \Firebase\JWT\JWT;
+
 class JwtController extends Zend_Controller_Action
 {
+    // https://www.techiediaries.com/php-jwt-authentication-tutorial/
     public function indexAction() {
-        die("learning about jwt");
+        
+        $secret_key = "declan*learning*jwt";
+        
+        $now_seconds = time();
+        
+        $token = array(
+            "iss" => "http://zendcode.localhost",
+            "aud" => "http://zendcode.localhost",
+            "iat" => $now_seconds,
+            "nbf" => $now_seconds,
+            "exp" => $now_seconds + 60 * 60,
+            "data" => array(
+                "id" => 13270,
+                "firstname" => "Declan",
+                "lastname" => "Munroe",
+                "email" => "declan.munroe@ics.ie"
+        ));
+        
+        http_response_code(200);
+
+        $jwt = JWT::encode($token, $secret_key);
+        
+        $this->_helper->json(array("message" => "Successful login.","jwt" => $jwt));
+        
     }
 }
