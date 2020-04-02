1d960b74990a92363fc768d7bb577a894d3876aa
Declan <declan.munroe@ics.ie>
2020-04-02 12:41:14 +0100

    Test with postman

--- application/controllers/JwtController.php
+++ application/controllers/JwtController.php
@@ -5,7 +5,24 @@ use \Firebase\JWT\JWT;
 class JwtController extends Zend_Controller_Action
 {
     // https://www.techiediaries.com/php-jwt-authentication-tutorial/
-    public function indexAction() {
+    public function loginAction() {
+        
+        $formData = $this->getRequest()->getPost();
+        $id = (int) isset($formData['user_id']) ? $formData['user_id'] : 0;
+        
+        $loggedInUserData = [];
+        
+        $declanUserData = array('id' => 13270, 'email' => 'declan.munroe@ics.ie', 'bike' => 'vitus sommet');
+        $kevinUserData = array('id' => 15999, 'email' => 'kevin@gmail.com', 'bike' => 'norco dh');
+        
+        if ($id == 13270) {
+            $loggedInUserData = $declanUserData;
+        } else if ($id == 15999) {
+            $loggedInUserData = $kevinUserData;
+        } else {
+            http_response_code(401);
+            $this->_helper->json(array("message" => "Login failed","id" => $id));
+        }
         
         $secret_key = "declan*learning*jwt";
         
@@ -17,12 +34,8 @@ class JwtController extends Zend_Controller_Action
             "iat" => $now_seconds,
             "nbf" => $now_seconds,
             "exp" => $now_seconds + 60 * 60,
-            "data" => array(
-                "id" => 13270,
-                "firstname" => "Declan",
-                "lastname" => "Munroe",
-                "email" => "declan.munroe@ics.ie"
-        ));
+            "data" => $loggedInUserData
+        );
         
         http_response_code(200);
 
