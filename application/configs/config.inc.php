e0ff7e89e094632f03b810833bac50a11b172678
Declan <declan.munroe@ics.ie>
2020-04-20 18:12:34 +0100

    Update

--- application/configs/config.inc.php
+++ application/configs/config.inc.php
@@ -5,7 +5,7 @@ define("AWS_SECRET_KEY", getenv('AWS_SECRET_KEY'));
 define("STRIPE_PUBLIC_KEY", "pk_test_bKoEJzXJUAawQ3PNi3ZMCEcb");
 define("STRIPE_SECRET_KEY", "sk_test_6JC7BVfqGwhW6082I1ja9N7B");
 
-define("HOST", getenv('HOST'));
-define("DATABASE", getenv('DATABASE'));
-define("USER", getenv('USER'));
-define("PASSWORD", getenv('PASSWORD'));
+define("HOST", getenv('Server'));
+define("DATABASE", getenv('Database'));
+define("USER", getenv('Uid'));
+define("PASSWORD", getenv('Pwd'));
