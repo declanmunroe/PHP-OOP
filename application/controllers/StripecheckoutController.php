01becd917ea1b92297c2a85b3caf4bfc4f2bf34d
Declan <declan.munroe@ics.ie>
2020-05-19 15:50:32 +0100

    Test

--- application/controllers/StripecheckoutController.php
+++ application/controllers/StripecheckoutController.php
@@ -190,7 +190,7 @@ class StripecheckoutController extends Zend_Controller_Action
                     break;
 
                 default:
-                    die("Error on success action. Switch default hit. Type is " .var_dump($type));
+                    die("Error on success action. Switch default hit. Type is NULL or undefined");
             }
                 
             
