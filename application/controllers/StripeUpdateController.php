21a1ea071206caaf0406b15d22d8abaf2549b7c9
Declan <declan.munroe@ics.ie>
2020-09-14 14:50:21 +0100

    Test stripe update

--- application/controllers/StripeUpdateController.php
+++ application/controllers/StripeUpdateController.php
@@ -16,10 +16,11 @@ class StripeUpdateController extends Zend_Controller_Action
 
         $response_array = json_decode($response, true);
         
-        $intent = PaymentIntent::retrieve($response_array['data']['object']['payment_intent']);
+        $this->_helper->json($response_array);
+        //$intent = PaymentIntent::retrieve($response_array['data']['object']['payment_intent']);
         
-        $this->_helper->json($intent);
+        //$this->_helper->json($intent);
         
-        $this->_helper->json(array('unique_id' => $intent['charges']['data'][0]['metadata']['uniqueid'], 'payment_intent' => $intent['id'], 'type' => $intent['charges']['data'][0]['metadata']['customtype']));
+        //$this->_helper->json(array('unique_id' => $intent['charges']['data'][0]['metadata']['uniqueid'], 'payment_intent' => $intent['id'], 'type' => $intent['charges']['data'][0]['metadata']['customtype']));
     }
 }
