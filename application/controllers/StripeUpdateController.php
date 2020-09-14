cb240c58c2132fb512fdd7612098ad3473494ae6
Declan <declan.munroe@ics.ie>
2020-09-14 15:54:36 +0100

    Test stripe update

--- application/controllers/StripeUpdateController.php
+++ application/controllers/StripeUpdateController.php
@@ -23,4 +23,14 @@ class StripeUpdateController extends Zend_Controller_Action
         // status is still succeeded and all is still captured below
         $this->_helper->json(array('unique_id' => $intent['charges']['data'][0]['metadata']['uniqueid'], 'payment_intent' => $intent['id'], 'type' => $intent['charges']['data'][0]['metadata']['type']));
     }
+    
+    // Works with new version of stripe api 2020-08-27
+    public function paymentIntentWebhookAction()
+    {
+        $response = file_get_contents('php://input');
+
+        $response_array = json_decode($response, true);
+        
+        $this->_helper->json('declan');
+    }
 }
