435d2c2878a620bb44bd50b0e53b9e6d7b4ede27
Declan <declan.munroe@ics.ie>
2020-09-14 14:42:07 +0100

    Test stripe update

---
+++ application/controllers/StripeUpdateController.php
@@ -0,0 +1,25 @@
+<?php
+
+use Stripe\Stripe;
+use Stripe\PaymentIntent;
+
+class StripecheckoutController extends Zend_Controller_Action
+{
+    public function init()
+    {
+        Stripe::setApiKey(Application_Service_Config::getStripePrivateKey('skills'));
+    }
+    
+    public function checkoutWebhookAction() 
+    {
+        $response = file_get_contents('php://input');
+
+        $response_array = json_decode($response, true);
+        
+        $intent = PaymentIntent::retrieve($response_array['data']['object']['payment_intent']);
+        
+        $this->_helper->json($intent);
+        
+        $this->_helper->json(array('unique_id' => $intent['charges']['data'][0]['metadata']['uniqueid'], 'payment_intent' => $intent['id'], 'type' => $intent['charges']['data'][0]['metadata']['customtype']));
+    }
+}
